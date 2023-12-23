a3d662ee2b95915f3d2a924b5cd87e69c088dc92
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-12-24 00:05:22 +0600

    last commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Footer from '../components/Footer'
 const Home = () => {
   return (
     <div>
-      <img className="min-h-screen object-cover" src="/banner.jpg" alt="" />
+      <img className="min-h-screen object-cover" src="https://i.postimg.cc/259Jn6x0/front-view-cyber-monday-shopping-cart-with-bags-copy-space.jpg" alt="" />
       <Footer></Footer>
     </div>
   );
